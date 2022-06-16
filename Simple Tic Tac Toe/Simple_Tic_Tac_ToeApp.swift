c4232c65e087b72be83695fc431ab1a5861f923c
Burneikis <87457198+AlexBurneikis@users.noreply.github.com>
2022-06-16 11:55:30 +1000

    Initial Commit

---
+++ Simple Tic Tac Toe/Simple_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Tic_Tac_ToeApp.swift
+//  Simple Tic Tac Toe
+//
+//  Created by Alexander Burneikis on 16/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
